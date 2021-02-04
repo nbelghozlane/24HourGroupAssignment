b0bdccb699eebf87f645baa7edd986fdafa9aa53
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-02-03 21:05:10 -0500

    first commit

--- 24HourGroupAssignment.Data/Post.cs
+++ 24HourGroupAssignment.Data/Post.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,17 @@ namespace _24HourGroupAssignment.Data
     public class Post
     {
         //Amel
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid Author { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [ForeignKey(nameof(Comments))]
+        public int CommentId { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+       
     }
 }
---
+++ 24HourGroupAssignment.Models/PostCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourGroupAssignment.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Please enter at least 3 characters.")]
+        [MaxLength(20, ErrorMessage = "There are tooo many caracters in this field, 15 characters is the maximum")]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(20,ErrorMessage = "Please enter at least 3 characters.")]
+        [MaxLength(500,ErrorMessage = "There are tooo many caracters in this field, 15 characters is the maximum")]
+        public string Text { get; set; }
+
+    }
+}
