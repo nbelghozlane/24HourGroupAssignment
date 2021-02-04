3b193d7fc82e8e319dc8cee4f951598a37313758
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-02-03 21:56:36 -0500

    PostModels

---
+++ 24HourGroupAssignment.Models/PostEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourGroupAssignment.Models
+{
+    public class PostEdit
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int CommentId { get; set; }
+    }
+}
---
+++ 24HourGroupAssignment.Models/PostList.cs
@@ -0,0 +1,16 @@
+﻿using _24HourGroupAssignment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourGroupAssignment.Models
+{
+    public class PostList
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
