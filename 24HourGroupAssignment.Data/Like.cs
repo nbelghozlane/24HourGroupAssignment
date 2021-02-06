7558a4de1058f096e362daa1ab6ef8a010673713
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-02-05 22:28:51 -0500

    fixed branch

---
+++ 24HourGroupAssignment.Data/Like.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourGroupAssignment.Data
+{
+    public class Like
+    {
+        [Key]
+        public int LikeId { get; set; }
+        public Guid OwnerId { get; set; }
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+        [Required]
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ 24HourGroupAssignment.Models/Post/PostList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourGroupAssignment.Models.Post
+{
+    public class PostList
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        // public virtual List<Comment> Comments { get; set; }
+        public int Likes { get; set; }
+    }
+}
